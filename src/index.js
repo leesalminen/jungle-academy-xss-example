3f3f85981a1b386372fb149ad7b03db2fadee854
Lee Salminen <leesalminen@gmail.com>
2022-07-14 11:55:43 -0600

    asdf

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill'
 import { initializeApp } from "firebase/app"
-import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore/lite'
+import { getFirestore, collection, getDocs, query, where, onSnapshot, addDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDKlGhb8voPdKxCfEI-7KC6zj9PoU7itUo",
@@ -14,168 +14,40 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
-const getUserProvidedLocations = async () => {
-    const locationsCol = collection(db, 'locations')
-    const q = query(locationsCol, where("approved", "==", true))
-    const locationSnapshot = await getDocs(q)
-    const locationList = locationSnapshot.docs.map(doc => {
-        const data = doc.data()
+const listEl = document.getElementById('list')
+const formEl = document.getElementById('addForm')
 
-        return {
-            username: null,
-            acceptsLightning: data.acceptsLightning,
-            acceptsOnChain: data.acceptsOnChain,
-            acceptsLiquid: data.acceptsLiquid,
-            mapInfo: {
-                title: data.name,
-                coordinates: {
-                    latitude: data.latLong._lat,
-                    longitude: data.latLong._long,
-                }
-            }
-        }
-    })
- 
-    addMapPins(locationList)
-}
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-const getBitcoinJungleLocations = () => {
-    const body = JSON.stringify({
-        "query": "query businessMapMarkers { businessMapMarkers { username mapInfo { title coordinates { longitude latitude } } } }",
-        "variables": {},
-        "operationName": "businessMapMarkers"
-    })
-
-    fetch(
-        "https://api.mainnet.bitcoinjungle.app/graphql", 
-        {
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-            },
-            body: body,
-        }
-    )
-    .then((res) => res.json())
-    .then((obj) => {
-        const pins = obj.data.businessMapMarkers.map((pin) => {
-            return {
-                username: pin.username,
-                acceptsLightning: true,
-                acceptsOnChain: true,
-                acceptsLiquid: false,
-                mapInfo: pin.mapInfo,
-            } 
-        })
-
-        addMapPins(pins)
-    })
-}
-
-const addMapPins = (pins) => {
-    const markerAnnotations = pins.map((el) => {
-
-        const calloutDelegate = {
-            calloutContentForAnnotation: function() {
-                var element = document.createElement("div");
-                element.className = "review-callout-content";
-                var title = element.appendChild(document.createElement("h1"));
-                title.textContent = el.mapInfo.title;
-                
-                if(el.acceptsOnChain) {
-                    var img = element.appendChild(document.createElement("img"));
-                    img.src = "https://storage.googleapis.com/bitcoin-jungle-maps-images/onchain.png"
-                    img.width = 20
-                    img.style.display = "inline"
-                }
-
-                if(el.acceptsLightning) {
-                    var img = element.appendChild(document.createElement("img"));
-                    img.src = "https://storage.googleapis.com/bitcoin-jungle-maps-images/lightning.png"
-                    img.width = 20
-                    img.style.display = "inline"
-                }
-
-                if(el.acceptsLiquid) {
-                    var img = element.appendChild(document.createElement("img"));
-                    img.src = "https://storage.googleapis.com/bitcoin-jungle-maps-images/liquid.png"
-                    img.width = 20
-                    img.style.display = "inline"
-                }
-
-                return element;
-            },
-            calloutRightAccessoryForAnnotation: function() {
-                if(el.username) {
-                    const accessoryViewRight = document.createElement("a");
-                    accessoryViewRight.className = "right-accessory-view";
-                    accessoryViewRight.href = "https://pay.bitcoinjungle.app/" + el.username;
-                    accessoryViewRight.target = "_blank";
-                    accessoryViewRight.appendChild(document.createTextNode("➡"));
-
-                    return accessoryViewRight;
-                } else {
-                    const accessoryViewRight = document.createElement("a");
-                    accessoryViewRight.className = "right-accessory-view";
-                    accessoryViewRight.href = "#";
-
-                    return accessoryViewRight;
-                }
-            }
-        }
-
-
-        const coordinate = new mapkit.Coordinate(el.mapInfo.coordinates.latitude, el.mapInfo.coordinates.longitude)
-
-        let annotationObj = {
-            title: el.mapInfo.title,
-            callout: calloutDelegate,
-            titleVisibility: mapkit.FeatureVisibility.Hidden,
-        }
-
-        if(!el.username) {
-            annotationObj.color = "purple"
-        }
-
-        return new mapkit.MarkerAnnotation(coordinate, annotationObj)
-    })
-
-    map.addAnnotations(markerAnnotations)
-}
-
-mapkit.init({
-    authorizationCallback: function (done) {
-        fetch("/api/token")
-        .then((res) => res.text())
-        .then(done)
-        .then(getBitcoinJungleLocations)
-        .then(getUserProvidedLocations)
-    },
-    language: navigator.language || navigator.userLanguage,
+    addComment()
 })
 
-const center = new mapkit.Coordinate(9.1549238, -83.7570566)
-const map = new mapkit.Map("apple-maps", {
-    center,
-    cameraDistance: 50000,
-    showsPointsOfInterest: false,
-})
+const addListener = async () => {
+    const commentsCol = collection(db, 'comments');
+    const q = query(commentsCol)
 
-map.addEventListener('zoom-end', (evt) => {
-    const map = mapkit.maps[0]
-    const curentCameraDistance = map.cameraDistance.toFixed(0)
+    const observer = onSnapshot(q, (querySnapshot => {
+        listEl.innerHTML = ''
 
-    if(curentCameraDistance < 10000) {
-        map.annotations = map.annotations.map((el) => {
-            el.titleVisibility = mapkit.FeatureVisibility.Visible
+        const commentsList = querySnapshot.docs.map(doc => {
+            const comment = doc.data()
 
-            return el
+            const itemEl = document.createElement('li')
+            itemEl.classList.add("p-3");
+            console.log(comment.comment)
+            itemEl.innerHTML = comment.comment
+
+            listEl.append(itemEl)
         })
-    } else {
-        map.annotations = map.annotations.map((el) => {
-            el.titleVisibility = mapkit.FeatureVisibility.Hidden
+    }))
+}
 
-            return el
-        })
-    }
-})
+const addComment = async () => {
+    const comment = document.getElementById('add').value
+    const docRef = await addDoc(collection(db, "comments"), {comment: comment})
+
+    comment.value = ''
+}
+
+addListener()
